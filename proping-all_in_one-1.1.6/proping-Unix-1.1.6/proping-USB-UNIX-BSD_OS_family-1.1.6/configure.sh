6dc2cb92f78ba82a83a234bca095740b75bcceb6
jfbellemare2011 <root@localhost>
2015-02-23 20:45:58 -0500

    5e

---
+++ proping-all_in_one-1.1.6/proping-Unix-1.1.6/proping-USB-UNIX-BSD_OS_family-1.1.6/configure.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#			   ██▓███   ██▀███   ▒█████   ██▓███   ██▓ ███▄    █   ▄████ 
+#			  ▓██░  ██▒▓██ ▒ ██▒▒██▒  ██▒▓██░  ██▒▓██▒ ██ ▀█   █  ██▒ ▀█▒
+#			  ▓██░ ██▓▒▓██ ░▄█ ▒▒██░  ██▒▓██░ ██▓▒▒██▒▓██  ▀█ ██▒▒██░▄▄▄░
+#			  ▒██▄█▓▒ ▒▒██▀▀█▄  ▒██   ██░▒██▄█▓▒ ▒░██░▓██▒  ▐▌██▒░▓█  ██▓
+#			  ▒██▒ ░  ░░██▓ ▒██▒░ ████▓▒░▒██▒ ░  ░░██░▒██░   ▓██░░▒▓███▀▒
+#			  ▒▓▒░ ░  ░░ ▒▓ ░▒▓░░ ▒░▒░▒░ ▒▓▒░ ░  ░░▓  ░ ▒░   ▒ ▒  ░▒   ▒ 
+#			  ░▒ ░       ░▒ ░ ▒░  ░ ▒ ▒░ ░▒ ░      ▒ ░░ ░░   ░ ▒░  ░   ░ 
+#			  ░░         ░░   ░ ░ ░ ░ ▒  ░░        ▒ ░   ░   ░ ░ ░ ░   ░ 
+#			              ░         ░ ░            ░           ░       ░ 
+#	
+gcc -pthread ./src/proping.c ./src/tracert.c -o proping 2> ./err.txt
+cp ./src/proping.conf ./proping.conf 2> ./err.txt
+
+echo C compiling ./src/proping.c ./src/tracert.c ./src/ping.c to ./proping \n
+echo Copy of ./src/proping.conf TO ./proping.conf \n
